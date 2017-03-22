fe64156a8a4b0bc0d438a50565b789fc5b5cdc1a
allen30331 <allen30331@gmail.com>
2017-03-22 16:12:43 -0400

    created post schema

---
+++ models.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+
+const postSchema = mongoose.Schema({
+	title: {type: String, required: true},
+	categories: {type: String, required: true},
+	content: {type: String, required: true}
+});
+
+postSchema.methods.apiRepr = function() {
+	return {
+		id: this._id,
+		title: this.title,
+		content: this.content
+	}
+}
+
+const Post = mongoose.model('Posts', postSchema);
+
+module.exports = {Post};
