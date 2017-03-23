a2cbd5b8fec76af2414f1b089cfab8b387c673ca
allen30331 <allen30331@gmail.com>
2017-03-23 13:57:10 -0400

    added tear down database function to tests

--- test/test.js
+++ test/test.js
@@ -8,14 +8,31 @@ const {TEST_DATABASE_URL} = require('../config');
 
 
 
+
+function tearDownDb() {
+  return new Promise((resolve, reject) => {
+    console.warn('Deleting database');
+    mongoose.connection.dropDatabase()
+      .then(result => resolve(result))
+      .catch(err => reject(err));
+  });
+}
+
+
 describe('Tests root url', function() {
 	before(function() {
 		return runServer(TEST_DATABASE_URL);
 	});
 
+
+	afterEach(function() {
+		return tearDownDb();
+	});
+
+
 	after(function() {
 		return closeServer();
-	})
+	});
 
 
 
