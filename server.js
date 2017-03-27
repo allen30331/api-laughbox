b264797f77709c0d00b460d29acc90ed8f6719a0
allen30331 <allen30331@gmail.com>
2017-03-27 19:34:32 -0400

    added delete and put to header

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(morgan('common'));
 app.use(bodyParser.json());
 app.use(function(req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
     next();
 });
