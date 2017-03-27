6b0577a9fdeb404858b86e746a2b8737002e7a7b
allen30331 <allen30331@gmail.com>
2017-03-27 19:27:22 -0400

    added set header

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ const {DATABASE_URL, PORT} = require('./config');
 app.use(express.static('public'));
 app.use(morgan('common'));
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
+    next();
+});
 app.use('/posts', postsRouter);
 
 
